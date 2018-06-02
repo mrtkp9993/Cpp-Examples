0f75b70701595ee48dd64086b07c1f6f4f0723f4
Murat Koptur <mkoptur3@gmail.com>
2018-06-02 15:33:46 +0300

    update Simpson's Rule

--- CompositeSimpsonsRule/CompositeSimpsonsRule/CompositeSimpsonsRule.cpp
+++ CompositeSimpsonsRule/CompositeSimpsonsRule/CompositeSimpsonsRule.cpp
@@ -9,14 +9,14 @@
 #include "CompositeSimpsonsRule.hpp"
 
 double compositeSimpsons(double a, double b, unsigned long n, std::function<double(double)> func) {
-    double X;
-    double XI;
+    double X = 0.0;
+    double XI = 0.0;
     // STEP 1
     double h = (b - a) / n;
     // STEP 2
     double XI0 = func(a) + func(b);
-    double XI1 = 0;
-    double XI2 = 0;
+    double XI1 = 0.0;
+    double XI2 = 0.0;
     // STEP 3
     for (int i = 1; i <= n - 1; i++) {
         // STEP 4
