dbe25fb0dbf9950878906ef4785e15b5e8eadf44
Murat Koptur <mkoptur3@gmail.com>
2018-06-08 21:59:41 +0300

    Decompositions added

--- EigenExamples/EigenExamples/main.cpp
+++ EigenExamples/EigenExamples/main.cpp
@@ -56,5 +56,33 @@ int main(int argc, const char * argv[]) {
     // Trace
     std::cout << "Trace of A = " << std::endl << A.trace() << std::endl;
     
+    // QR Decomposition
+    Eigen::FullPivHouseholderQR<Eigen::MatrixXd> qr_decomp(A);
+    Eigen::MatrixXd qr_Q = qr_decomp.matrixQ();
+    Eigen::MatrixXd qr_R = qr_decomp.matrixQR();
+    std::cout << "QR Decomposition of A " << std::endl;
+    std::cout << "Q = " << std::endl << qr_Q << std::endl;
+    std::cout << "R = " << std::endl << qr_R << std::endl;
+    
+    // LU Decomposition
+    Eigen::FullPivLU<Eigen::MatrixXd> lu_decomp(A);
+    Eigen::MatrixXd lu_P = lu_decomp.matrixLU();
+    Eigen::MatrixXd lu_L = lu_decomp.permutationP();
+    Eigen::MatrixXd lu_U = lu_decomp.permutationQ();
+    std::cout << "LU Decomposition of A " << std::endl;
+    std::cout << "P = " << std::endl << lu_P << std::endl;
+    std::cout << "L = " << std::endl << lu_L << std::endl;
+    std::cout << "U = " << std::endl << lu_U << std::endl;
+    
+    // Singular Value Decomposition
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::MatrixXd svd_U = svd.matrixU();
+    Eigen::MatrixXd svd_S = svd.singularValues().asDiagonal();
+    Eigen::MatrixXd svd_V = svd.matrixV().transpose();
+    std::cout << "SVD  of A " << std::endl;
+    std::cout << "U = " << std::endl << svd_U << std::endl;
+    std::cout << "S = " << std::endl << svd_S << std::endl;
+    std::cout << "V = " << std::endl << svd_V << std::endl;
+    
     return 0;
 }
