863140c06646a728d3fbc2b9e0e384d60c49b501
Murat Koptur <mkoptur3@gmail.com>
2018-06-10 20:34:30 +0300

    Fixed-point iteration added

---
+++ FixedPointIteration/FixedPointIteration/FixedPointIteration.hpp
@@ -0,0 +1,19 @@
+//
+//  FixedPointIteration.hpp
+//  FixedPointIteration
+//
+//
+//  Algorithm 2.2 (Numerical Analysis, Ninth Edition, Burden, Faires)
+//  Fixed-Point Iteration
+
+#ifndef FixedPointIteration_hpp
+#define FixedPointIteration_hpp
+
+#include <stdio.h>
+#include <cmath>
+#include <functional>
+#include <iostream>
+
+double fixedPointIter(double p0, double tol, unsigned long N0, std::function<double(double)> g);
+
+#endif /* FixedPointIteration_hpp */
