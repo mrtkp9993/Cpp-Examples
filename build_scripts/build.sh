e6332b36d1db83f9befdfbdfe03c1b0cd1a9eed0
Murat Koptur <mkoptur3@gmail.com>
2018-06-24 14:28:58 +0300

    ci script edited

---
+++ build_scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ "`uname`" == "Darwin" ]]; then
+  xcodebuild -project ../polyroot/polyroot.xcodeproj -scheme polyroot
+  xcodebuild -project ../finiteDifferences/finiteDifferences.xcodeproj -scheme finiteDifferences
+  xcodebuild -project ../CubicSplineInterpolation/CubicSplineInterpolation.xcodeproj -scheme CubicSplineInterpolation
+  xcodebuild -project ../CompositeSimpsonsRule/CompositeSimpsonsRule.xcodeproj -scheme CompositeSimpsonsRule
+  xcodebuild -project ../EigenExamples/EigenExamples.xcodeproj -scheme EigenExamples
+  xcodebuild -project ../FixedPointIteration/FixedPointIteration.xcodeproj -scheme FixedPointIteration
+  xcodebuild -project ../RandomNumberGenerators/RandomNumberGenerators.xcodeproj -scheme RandomNumberGenerators
+  xcodebuild -project ../RungeKutta/RungeKutta.xcodeproj -scheme RungeKutta
+  xcodebuild -project ../GoldenSectionSearch/GoldenSectionSearch.xcodeproj -scheme GoldenSectionSearch
+fi
