976f565a536e505096921580f4f53fcc749a93b9
Murat Koptur <mkoptur3@gmail.com>
2019-01-13 21:21:15 +0300

    BBP Type Formula added

---
+++ BBPTypeFormula/BBPTypeFormula.h
@@ -0,0 +1,10 @@
+#ifndef BBPTYPEFORMULA_BBPTYPEFORMULA_H
+#define BBPTYPEFORMULA_BBPTYPEFORMULA_H
+
+#include <cassert>
+#include <cmath>
+#include <vector>
+
+long double P(const int & s, const int & b, const int & n, const std::vector<int> &A, const int & n_iter);
+
+#endif //BBPTYPEFORMULA_BBPTYPEFORMULA_H
