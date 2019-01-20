44b8316cf350320e37d6bb33f29892a47f90036d
Murat Koptur <mkoptur3@gmail.com>
2019-01-20 12:51:30 +0300

    JB test added for normality

--- src/metropolisHastings.h
+++ src/metropolisHastings.h
@@ -7,6 +7,7 @@
 #include <ctime>
 #include <functional>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 double runif(const double& a = 0, const double& b = 1);
@@ -14,5 +15,6 @@ double rnorm(const double& mean = 0, const double& sd = 1);
 std::vector<double> generate(const std::function<double(double)>& target,
                              const unsigned int& count,
                              const unsigned int& burn_in_period);
-
+double pchisq(const double& stat, const unsigned int& k);
+bool jb_test(const std::vector<double>& x);
 #endif  // METROPOLISHASTINGS_H
