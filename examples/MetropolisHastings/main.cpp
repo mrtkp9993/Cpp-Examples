07e34b910e8a3c7fa016baabdbda06234c81d76b
Murat Koptur <mkoptur3@gmail.com>
2019-01-20 11:49:48 +0300

    Create main.cpp

---
+++ examples/MetropolisHastings/main.cpp
@@ -0,0 +1,12 @@
+#include "../../src/metropolisHastings.h"
+
+double target(const double& x) { return x < 0 ? 0 : std::exp(-x); }
+
+int main() {
+  std::vector<double> nums = generate(target, 10000, 2500);
+  for (auto num : nums) {
+    std::cout << num << std::endl;
+  }
+
+  return 0;
+}
